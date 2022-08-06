8f7168a92f5b6ff7819d857cdd9df959d469935e
George Kikas <kikasgeorg@ece.auth.gr>
2022-08-06 11:00:58 +0000

    Add SPS-configurable GMSK filter taps

--- inc/GMSKTranscoder.hpp
+++ inc/GMSKTranscoder.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <FMTranscoder.hpp>
+#include "GMSKFilterTaps.hpp"
 #include <cmath>
 #include <iostream>
 
@@ -12,29 +13,35 @@ struct PLLParameters{
     double G2; // Gain of the lower branch of the loop filter
 };
 
-class GMSKTranscoder {
+
+template <int samplesPerSymbol> class GMSKTranscoder {
 private:
     double internalBufferInPhase[60000]; // TODO: Determine size (signal_length * max_samplesPerSymbol)
     double internalBufferQuadrature[60000];
     double wienerTaps[3] = {-0.0859984, 1.0116342, -0.0859984};
-    double delayedTaps[60];    // TODO: Determine size as 6 * samples/symbol, here max samples/symbol considered 10
+    double delayedTaps[6 * samplesPerSymbol];
     bool equalize;
     double convolvedFilters[109]; //TODO: Length is the length of gmsk_mod and delayed taps added (Now 60 + 49)
     uint32_t samplingFrequency;
     uint32_t samplingRate;
-    uint32_t samplesPerSymbol;
     uint32_t maxFrequency;
     uint32_t symbolRate;
     uint32_t maxDeviation;
     FMTranscoder fmTranscoder;
     PLLParameters pllParams;
+    const double *gmskModulationCoefficients = nullptr;
+
+    /**
+     * Enumeration of samples per symbol values
+     */
+    enum SPS {SPS_4 = 4, SPS_6 = 6, SPS_10 = 10};
 
 public:
     GMSKTranscoder(uint32_t sampleFrequency, uint32_t symbRate, bool equalize) :
             fmTranscoder(FMTranscoder(sampleFrequency, symbRate / 2, 0,
                                       symbRate / 4, 0, 0)),
             symbolRate(symbRate), samplingFrequency(sampleFrequency) {
-        samplesPerSymbol = sampleFrequency / symbolRate;
+
         maxFrequency = symbolRate / 2;
         maxDeviation = symbolRate / 4;
 
@@ -56,6 +63,20 @@ public:
                                     g1*M_PI/2,
                                     g2*M_PI/2
                                     };
+
+        switch (samplesPerSymbol) {
+            case SPS_4:
+                gmskModulationCoefficients = GMSKSPS4BTs25;
+                break;
+            case SPS_6:
+                gmskModulationCoefficients = GMSKSPS6BTs25;
+                break;
+            case SPS_10:
+                gmskModulationCoefficients = GMSKSPS10BTs25;
+                break;
+            default:
+                std::cerr << "Value Error: samplesPerSymbol value is not supported\n";
+        }
     }
 
     // TODO: signal_length should be a pre-determined number
