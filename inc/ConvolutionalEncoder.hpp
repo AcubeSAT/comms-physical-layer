9f3dd07922a53081878a52cde8e033b9ca562b6f
Christoforos Tsiolakis <chris.tsiol@gmail.com>
2022-08-13 15:41:46 +0000

    Changed bool* to etl::bitset in LDPC and Convolutional Encoder - Added LDPC Encoder test.

--- inc/ConvolutionalEncoder.hpp
+++ inc/ConvolutionalEncoder.hpp
@@ -1,36 +1,44 @@
 #pragma once
 
+#include "etl/bitset.h"
 #include <stdint.h>
 
 class ConvolutionalEncoder {
-protected:
-    /**
-     * @param generator The generator polynomials used for the encoding, but inversed
-     */
-    inline static const bool generator[2][7] = {{1, 0, 0, 1, 1, 1, 1}, {1, 1, 0, 1, 1, 0, 1}};
+  public:
     /**
      * @param rate The number of the generator polynomials
      */
-    static const uint16_t rate = 2;
+    static constexpr uint8_t rate = 2;
     /**
      * @param constraintLength The length of each generator polynomial
      */
-    static const uint8_t constraintLength = 7;
+    static constexpr uint8_t constraintLength = 7;
     /**
-     * @param decodedCodewordLength The length of the encoded codeword
+     * @param generator The generator polynomials used for the encoding, but inversed
      */
-    static const uint16_t encodedCodewordLength = 4096;
-
-public:
-    uint16_t inputItemsNumber;
+    inline static const bool generator[rate][constraintLength] = {{1, 0, 0, 1, 1, 1, 1},
+                                                                  {1, 1, 0, 1, 1, 0, 1}};
     /**
-     * Initializes an object of the class and sets the output to be a multiple of the encodedCodewordLength.
+     * @param inputLength The length of the input
+     */
+    static constexpr uint16_t inputLength = 2048;
+    /**
+     * @param outputLength The length of the encoded codeword
+     */
+    static constexpr uint16_t outputLength = inputLength * rate;
+
+    /**
+     * Initializes an object of the class.
      */
     ConvolutionalEncoder();
-    /*!
+    /**
      * Destroys the object of the class.
      */
     ~ConvolutionalEncoder();
-
-    void encodeMessage(const bool* const inputMessageAddress, bool* const outputMessageAddress);
+    /**
+     * The encoding method
+     * @param input Reference to bit packed byte-encoded data (etl::<bitset>) of size inputLength
+     * @param output Reference to bit packed byte-encoded data (etl::<bitset>) of size outputLength
+     */
+    void encode(const etl::bitset<inputLength>& input, etl::bitset<outputLength>& output);
 };
