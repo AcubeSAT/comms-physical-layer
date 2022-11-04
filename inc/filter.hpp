566e19a8396478cce5c865ec7700f4302beb1381
Sedictious <ele.hatzy@gmail.com>
2022-11-04 18:37:11 +0000

    Add I/Q transceiver  packetization

--- inc/filter.hpp
+++ inc/filter.hpp
@@ -3,12 +3,20 @@
 #include <cmath>
 #include <cstdint>
 
-// Filtering is supposed to be architecture-specific. When porting to STM32 make sure to make use of the existing HAL
-// library functions (etc `LL_FMAC_FUNC_CONVO_FIR`)
+constexpr uint8_t inPhaseSync = 0b10;
+constexpr uint8_t quadratureSync = 0b01;
 
-void filterFIR(const double *filterTaps, uint16_t numberOfTaps, const double *inputSignal, uint16_t size,
+// Filtering is supposed to be architecture-specific. When porting to STM32 make
+// sure to make use of the existing HAL library functions (etc
+// `LL_FMAC_FUNC_CONVO_FIR`)
+
+void filterFIR(const double *filterTaps, uint16_t numberOfTaps,
+               const double *inputSignal, uint16_t size, double *outputSignal);
+
+void integrate(const double *inputSignal, uint16_t size, uint16_t numberOfTaps,
                double *outputSignal);
 
-void integrate(const double *inputSignal, uint16_t size, uint16_t numberOfTaps, double *outputSignal);
-
 void multiplyVector(double *signal, uint16_t length, double coef);
+
+void packetizeIQ(double *inPhaseSignal, double *quadratureSignal,
+                 uint16_t length, uint32_t *outputSignal);
