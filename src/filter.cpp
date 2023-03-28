d2823f80a0459c394bd6382239bbe5b523b91933
Giannis <giannis.dimoulios@gmail.com>
2023-03-28 14:24:47 +0300

    baby steps: implement firFilter and multiplyVector using the respective cmsis functions

--- src/filter.cpp
+++ src/filter.cpp
@@ -1,20 +1,31 @@
 #include "filter.hpp"
+// extern "C"  is due to some stupid bug with nested externs in Drivers/CMSIS/Include/cmsis_gcc.h
+// This is the dummy solution, other solutions may be
+// a. updating cmsis header files to >= 5.7 or
+// b. moving lines 133 - 149 of the header mentioned above out of the function they are currently in, making them global.
+extern "C" {
+    #include "arm_math.h"
+}
 
 void filterFIR(const double *filterTaps, uint16_t numberOfTaps,
                const double *inputSignal, uint16_t size, double *outputSignal) {
-    // TODO: Naive implementation for testing purposes only! If this is mean to
-    // be run also on ground, replace with a
-    //  well-known library or at least a more efficient, vectorised
-    //  implementation. Otherwise, fall back to architecture-specific libraries.
     //  Additionally, if the latter is the case, an additional parameter needs
     //  to be passed both here and in the initialization of the modems that
     //  defines which implementation is to be chosen
+#ifndef STM32
     for (uint16_t n = 0; n < size; n++) {
         outputSignal[n] = 0;
         for (uint16_t k = 0; k < fmin(n + 1, numberOfTaps); k++) {
             outputSignal[n] += filterTaps[k] * inputSignal[n - k];
         }
     }
+#else
+    arm_fir_instance_f32 fir_inst;
+    // find max size and max numberOfTaps to make firState static
+    float firState[size + numberOfTaps - 1];
+    arm_fir_init_f32(&fir_inst, numberOfTaps, (float *)filterTaps, firState, size);
+    arm_fir_f32(&fir_inst, (float *)inputSignal, (float *)outputSignal, size);
+#endif
 }
 
 // TODO: Instead of O(nk) this can easily be simplified to O(n) by considering
@@ -34,9 +45,13 @@ void integrate(const double *inputSignal, uint16_t size, uint16_t numberOfTaps,
 void multiplyVector(double *signal, uint16_t length, double coef) {
     // TODO: This is a placeholder bad implementation. Replace with HAL function
     // (interface may need to change slightly)
+#ifndef STM32
     for (int i = 0; i < length; i++) {
         signal[i] *= coef;
     }
+#else
+    arm_scale_f32((float *)signal, coef, (float *)signal, length);
+#endif
 }
 
 /**
