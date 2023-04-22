1ce36da92af80daf7eb738a0f09bdbf7ea1b8663
Giannis <giannis.dimoulios@gmail.com>
2023-04-23 01:52:29 +0300

    use a more realistic value for the size of the fir buffer

--- src/filter.cpp
+++ src/filter.cpp
@@ -24,7 +24,8 @@ void filterFIR(const float *filterTaps, uint16_t numberOfTaps,
 #else
     arm_fir_instance_f32 firInst;
     // find max size and max numberOfTaps to make firState static
-    static float firState[4000];
+    static float firState[13000];
+//    float firState[size + numberOfTaps - 1];
     arm_fir_init_f32(&firInst, numberOfTaps, filterTaps, firState, size);
     arm_fir_f32(&firInst, inputSignal, outputSignal, size);
 #endif
