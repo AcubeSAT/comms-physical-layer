42e744ff143ba2bc3a8b5532d8343c4ab1abd0ae
Giannis <giannis.dimoulios@gmail.com>
2023-05-02 03:09:11 +0300

    test gmsk modulation on x86 using the same input signal

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,33 @@
 #include "OQPSKTranscoder.hpp"
 
 #include <iostream>
+#include <string>
 
 int main(void) {
-  return 0;
+    freopen("../test/iofiles/gmsk_input_2048.txt", "r", stdin); 
+    freopen("../test/iofiles/gmsk_float_x86_output.txt", "w", stdout); 
+
+    const int length = 2048;
+    const int sps = 6;
+    float inPhaseOut[sps * length] = {0};
+    float quadratureOut[sps * length] = {0};
+
+    std::string input;
+    std::cin >> input; 
+
+    etl::bitset<length> inputSignal;
+    for (int i = 0; i < length; i++) {
+        inputSignal.set(i, input[i] == 1); 
+    }
+
+    GMSKTranscoder<sps, length> gmskTrans(48000, 8000, false);
+    gmskTrans.modulate(inputSignal, inPhaseOut, quadratureOut);
+
+    for (int i = 0; i < sps * length; i++) {
+        std::cout << inPhaseOut[i] << std::endl;
+        std::cout << quadratureOut[i] << std::endl;
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
