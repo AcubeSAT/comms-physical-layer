3ba0e21c6e86e7f3308f94fd6012c336023671c6
Giannis <giannis.dimoulios@gmail.com>
2023-05-04 10:11:03 +0300

    testing x86 modulation

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,21 @@
 #include "OQPSKTranscoder.hpp"
 
 #include <iostream>
+#include <fstream> 
 #include <string>
 #include <cstdint>
 
 int main(void) {
-    freopen("../test/iofiles/gmsk_input_2048.txt", "r", stdin); 
-    freopen("../test/iofiles/gmsk_float_x86_output.txt", "w", stdout); 
+    std::ifstream file("../test/iofiles/gmsk_input_2048.txt");
+    std::ofstream iqFile("../test/iofiles/gmsk_float_x86_output.txt"); 
+
+    if (!file.is_open()) {
+        std::cout << "Cannot open input" << std::endl; 
+    }
+
+    if (!iqFile.is_open()) {
+        std::cout << "Cannot open output" << std::endl; 
+    }
 
     const uint16_t length = 2048;
     const uint8_t sps = 6;
@@ -15,9 +24,10 @@ int main(void) {
     float quadratureOut[sps * length] = {0};
 
     std::string input;
-    std::cin >> input; 
+    file >> input; 
 
     etl::bitset<length> inputSignal;
+    etl::bitset<length> outputSignal;
     for (int i = 0; i < length; i++) {
         inputSignal.set(i, input[i] == '1');
     }
@@ -27,10 +37,25 @@ int main(void) {
     gmskTrans.modulate(inputSignal, inPhaseOut, quadratureOut);
 
     for (int i = 0; i < sps * length; i++) {
-        std::cout << inPhaseOut[i] << std::endl;
-        std::cout << quadratureOut[i] << std::endl;
-        std::cout << std::endl;
+        iqFile << inPhaseOut[i] << std::endl;
+        iqFile << quadratureOut[i] << std::endl;
+        iqFile << std::endl;
     }
 
+    gmskTrans.demodulate(inPhaseOut, quadratureOut, length, outputSignal);
+    int optimalErrors = length;
+    int shift = 0;
+    for (int i = 0; i < length; i++) {
+        int errors = 0;
+        for (int j = i; j < inputSignal.size(); j++) {
+            errors += outputSignal[j] ^ inputSignal[j - i];
+        }
+        if (errors < optimalErrors) {
+            optimalErrors = errors;
+            shift = i;
+        }
+    }
+
+    printf("%d %d\n", optimalErrors, shift);
     return 0;
 }
